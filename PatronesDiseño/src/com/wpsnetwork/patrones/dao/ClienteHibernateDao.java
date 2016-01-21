60bb6f6200226da41460066af5ccba572115790e
marcrevenga <marc.revenga@gmail.com>
2016-01-21 09:54:55 +0100

    Dao y wrapper

---
+++ PatronesDiseño/src/com/wpsnetwork/patrones/dao/ClienteHibernateDao.java
@@ -0,0 +1,39 @@
+package com.wpsnetwork.patrones.dao;
+
+import java.util.List;
+
+import com.wpsnetwork.patrones.dao.interfaces.ClienteDao;
+
+public class ClienteHibernateDao implements ClienteDao {
+
+	@Override
+	public List<Cliente> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Cliente c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(Cliente c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cliente get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
