09661c2b0da90045b17f5a65f557b61a92792af4
marcrevenga <marc.revenga@gmail.com>
2016-02-22 09:12:30 +0100

    Ejercicio acabado

--- LlamadaProcedimientosYFunciones/src/com/wpsnetwork/consola/Main.java
+++ LlamadaProcedimientosYFunciones/src/com/wpsnetwork/consola/Main.java
@@ -1,7 +1,18 @@
 package com.wpsnetwork.consola;
 
 import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneOffset;
 import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import oracle.jdbc.pool.OracleDataSource;
 
 /*
 CREAMOS PROCEDIMIENTOS  (desde el SQL Plus*)
@@ -37,7 +48,7 @@ BEGIN
     RETURN VNOMBRE;
 END;
 /
-*/
+ */
 
 
 public class Main {
@@ -45,8 +56,13 @@ public class Main {
 	private static String usuario;
 	private static String driver;
 	private static String password;
+	private static DataSource datasource;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws 	IOException, 
+													InstantiationException, 
+													IllegalAccessException, 
+													ClassNotFoundException, 
+													SQLException {
 		
 		Properties recurso = new Properties();
 		recurso.load(Main.class.getResourceAsStream("../../../configuracionBD.properties"));
@@ -60,28 +76,35 @@ public class Main {
 		Class.forName(driver).newInstance();
 		
 		//Crearemos un datasource
+		OracleDataSource ds = new OracleDataSource();
+		ds.setUser(usuario);
+		ds.setPassword(password);
+		ds.setURL(url);
+		datasource = ds;
 		
+		try(Connection conexion = getConnection()){
+			//Llamada al procedimiento
+			CallableStatement comando = conexion.prepareCall("{call INSERTA_PEDIDO(?,?,?)}");
+			comando.setString(1, "Cliente 1");
+			long milis = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli();
+			comando.setDate(2, new Date(milis));
+			comando.setString(3, "Pedido nuevo");
+			comando.execute();
+			
+			//Llamada a la función
+			comando = conexion.prepareCall("{?=call NOMBRE_DE_CLIENTE(?)}");
+			comando.setInt(2, 1);  		//Queremos obtener el nombre del cliente con id = 1
+			comando.registerOutParameter(1, java.sql.Types.VARCHAR);
+			comando.execute();
+			System.out.println( comando.getString(1) );
+			
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	private static Connection getConnection() throws SQLException{
+		return datasource.getConnection();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
