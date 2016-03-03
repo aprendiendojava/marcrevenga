c0a53c2d99e5bb1fc78333d5d9838a9b2d38ddee
marcrevenga <marc.revenga@gmail.com>
2016-03-03 08:58:05 +0100

    JoinRowSet

--- RowSet/src/com/wpsnetwork/consola/Main.java
+++ RowSet/src/com/wpsnetwork/consola/Main.java
@@ -2,6 +2,7 @@ package com.wpsnetwork.consola;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Properties;
 import javax.sql.RowSet;
 import javax.sql.rowset.FilteredRowSet;
 import javax.sql.rowset.JdbcRowSet;
+import javax.sql.rowset.JoinRowSet;
 import javax.sql.rowset.Predicate;
 import javax.sql.rowset.RowSetFactory;
 import javax.sql.rowset.RowSetProvider;
@@ -91,29 +93,75 @@ public class Main {
 		//FilteredRowSet
 		//Permite sobre un mismo resultado, ir aplicando filtros para obtener diferentes
 		//  resultados parciales
-		FilteredRowSet rowSet = fabricador.createFilteredRowSet();		//Configuramos la conexion del RowSet
-		rowSet.setUrl(url);
-		rowSet.setUsername(usuario);
-		rowSet.setPassword(password);
-		rowSet.setKeyColumns(new int[]{1});
+//		FilteredRowSet rowSet = fabricador.createFilteredRowSet();		//Configuramos la conexion del RowSet
+//		rowSet.setUrl(url);
+//		rowSet.setUsername(usuario);
+//		rowSet.setPassword(password);
+//		rowSet.setKeyColumns(new int[]{1});
+//		
+//		rowSet.setCommand("SELECT ID, FECHA, INFO, IDCLIENTE FROM PEDIDO");
+//		rowSet.execute();
+//		
+//		//rowSet.setFilter(new FiltroRangoYear(2014, 2015));
+//		rowSet.setFilter(new FiltroRangoYear(2012, 2013));
+//		
+//		//Nos tenemos que posicionar en la posicion antes de la primera posición
+//		List<Pedido> pedidos = new ArrayList<>();
+//		rowSet.beforeFirst();
+//		
+//		while(rowSet.next()){
+//			pedidos.add(new Pedido(	rowSet.getInt("id"), 
+//									rowSet.getDate("fecha"), 
+//									rowSet.getString("info"),
+//									rowSet.getInt("idcliente")));
+//		}
+//		System.out.println(pedidos);
 		
-		rowSet.setCommand("SELECT ID, FECHA, INFO, IDCLIENTE FROM PEDIDO");
-		rowSet.execute();
+		//JoinRowSet
+		//Permite hacer join de dos rowsets
 		
-		rowSet.setFilter(new FiltroRangoYear(2014, 2015));
+		/*
+		 * SELECT p.id, p.fecha, p.info, p.idcliente, c.id, c.nombre
+		 * 	FROM cliente c JOIN pedido p ON c.id = p.idcliente;
+		 */
 		
-		//Nos tenemos que posicionar en la posicion antes de la primera posición
-		List<Pedido> pedidos = new ArrayList<>();
-		rowSet.beforeFirst();
+		JoinRowSet setPedidosCliente = fabricador.createJoinRowSet();
 		
-		while(rowSet.next()){
-			pedidos.add(new Pedido(	rowSet.getInt("id"), 
-									rowSet.getDate("fecha"), 
-									rowSet.getString("info"),
-									rowSet.getInt("idcliente")));
+		WebRowSet setClientes = fabricador.createWebRowSet();		//Configuramos la conexion del RowSet
+		setClientes.setUrl(url);
+		setClientes.setUsername(usuario);
+		setClientes.setPassword(password);
+		setClientes.setKeyColumns(new int[]{1});
+	
+		setClientes.setCommand("SELECT ID CODCLIENTE, NOMBRE FROM CLIENTE");
+		setClientes.execute();
+		
+		WebRowSet setPedidos = fabricador.createWebRowSet();		//Configuramos la conexion del RowSet
+		setPedidos.setUrl(url);
+		setPedidos.setUsername(usuario);
+		setPedidos.setPassword(password);
+		setPedidos.setKeyColumns(new int[]{1});
+	
+		setPedidos.setCommand("SELECT ID CODPEDIDO, FECHA, INFO, IDCLIENTE FROM PEDIDO");
+		setPedidos.execute();
+		
+		setPedidosCliente.addRowSet(setClientes, "CODCLIENTE");
+		setPedidosCliente.addRowSet(setPedidos, "IDCLIENTE");
+		
+		ResultSetMetaData meta = setPedidosCliente.getMetaData();	//Obtenemos los metadatos
+		int numColumnas = meta.getColumnCount();	//Número de columnas del resultado
+		
+		setPedidosCliente.beforeFirst();
+		while(setPedidosCliente.next()){
+			for(int i=1; i<= numColumnas; i++){
+				System.out.printf("%s = %s\n", meta.getColumnName(i), 
+												setPedidosCliente.getObject(i).toString());
+			}
 		}
-		System.out.println(pedidos);
 		
+		setClientes.close();
+		setPedidos.close();
+		setPedidosCliente.close();
 	}
 
 }
